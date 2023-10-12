dfc5694235d26442aee3bd28ff96cf84aca6b580
Silkou Maxim <maxim.silkou.2002@gmail.com>
2023-10-13 00:35:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+const formDef1 =
+    [
+        { label: 'Название сайта:', kind: 'longtext', name: 'sitename' },
+        { label: 'URL сайта:', kind: 'longtext', name: 'siteurl' },
+        { label: 'Посетителей в сутки:', kind: 'number', name: 'visitors' },
+        { label: 'E-mail для связи:', kind: 'shorttext', name: 'email' },
+        {
+            label: 'Рубрика каталога:', kind: 'combo', name: 'division',
+            variants: [{ text: 'здоровье', value: 1 }, { text: 'домашний уют', value: 2 }, { text: 'бытовая техника', value: 3 }]
+        },
+        {
+            label: 'Размещение:', kind: 'radio', name: 'payment',
+            variants: [{ text: 'бесплатное', value: 1 }, { text: 'платное', value: 2 }, { text: 'VIP', value: 3 }]
+        },
+        { label: 'Разрешить отзывы:', kind: 'check', name: 'votes' },
+        { label: 'Описание сайта:', kind: 'memo', name: 'description' },
+        { caption: 'Опубликовать', kind: 'submit' },
+    ];
+
+const formDef2 =
+    [
+        { label: 'Фамилия:', kind: 'longtext', name: 'lastname' },
+        { label: 'Имя:', kind: 'longtext', name: 'firstname' },
+        { label: 'Отчество:', kind: 'longtext', name: 'secondname' },
+        { label: 'Возраст:', kind: 'number', name: 'age' },
+        { caption: 'Зарегистрироваться', kind: 'submit' },
+    ];
+
+let firstForm = document.forms.firstForm;
+let secondForm = document.forms.secondForm;
+
+generateForm(firstForm, formDef1);
+
+function generateForm(form, elements){
+    console.log(form);
+
+    let elementsCopy = JSON.parse(JSON.stringify(elements));;
+
+    elementsCopy.forEach(element => {
+      let labelChildElement = document.createElement('label');
+      labelChildElement.innerHTML = element.label;
+      delete element.label;
+      form.appendChild(labelChildElement);
+
+      let childElement = document.createElement('input'); 
+
+      for (let key in element){
+        childElement.setAttribute(key, element[key]);
+      }
+
+      form.appendChild(childElement)
+
+      let brChildElement = document.createElement('br');
+      form.appendChild(brChildElement);
+    });
+}
